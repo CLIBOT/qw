393a055a4c0e024d2b1af5db7954fa2ea6504e29
httpomid <omidhttp1@gmail.com>
2016-07-03 11:09:40 +0430

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -1,39 +1,43 @@
 do
-local function callbackres(extra, success, result) -- Callback for res_user in line 27
-  local user = 'user#id'..result.id
+local function callbackres(extra, success, result)
+--vardump(result)
+  local user = 'user#id'..result.peer_id
 	local chat = 'chat#id'..extra.chatid
-	if is_banned(result.id, extra.chatid) then -- Ignore bans
-            send_large_msg(chat, 'User is banned.')
-	elseif is_gbanned(result.id) then -- Ignore globall bans
-	    send_large_msg(chat, 'User is globaly banned.')
+	local channel = 'channel#id'..extra.chatid
+	if is_banned(result.id, extra.chatid) then 
+        send_large_msg(chat, 'کاربر بن شده است.')
+        send_large_msg(channel, 'کاربر بن شده است.')
+	elseif is_gbanned(result.id) then
+	    send_large_msg(chat, 'کاربر بن گلوبال شده است.')
+		send_large_msg(channel, 'کاربر بن گلوبال شده است.')
 	else    
-	    chat_add_user(chat, user, ok_cb, false) -- Add user on chat
+	    chat_add_user(chat, user, ok_cb, false) 
+		channel_invite(channel, user, ok_cb, false)
 	end
 end
 function run(msg, matches)
   local data = load_data(_config.moderation.data)
+  if not is_momod(msg) then
+	return
+  end
+  if not is_admin1(msg) then -- For admins only !
+		return 'فقط ادمین میتواند دعوت کند.'
+  end
   if not is_realm(msg) then
-    if data[tostring(msg.to.id)] and data[tostring(msg.to.id)]['settings']['lock_member'] == 'yes' and not is_admin(msg) then
-		  return 'Group is private.'
+    if data[tostring(msg.to.id)]['settings']['lock_member'] == 'yes' and not is_admin1(msg) then
+		  return ' گروه خصوصی است لطفا ابتدا قفل اعضا را باز کنید.'
     end
   end
-  if msg.to.type ~= 'chat' then 
-    return
-  end
-  if not is_momod(msg) then
-    return
-  end
-  --if not is_admin(msg) then -- For admins only !
-    --return 'Only admins can invite.'
-  --end
-	local cbres_extra = {chatid = msg.to.id}
-  local username = matches[1]
-  local username = username:gsub("@","")
-  res_user(username,  callbackres, cbres_extra)
+	if msg.to.type ~= 'chat' or msg.to.type ~= 'channel' then 
+		local cbres_extra = {chatid = msg.to.id}
+		local username = matches[1]
+		local username = username:gsub("@","")
+		resolve_username(username,  callbackres, cbres_extra)
+	end
 end
 return {
     patterns = {
-      "^[!/]invite (.*)$"
+      "^[#]دعوت (.*)$"
     },
     run = run
 }
